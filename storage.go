58f5b817bed4384aadfb84d78b0a206a41f6271e
aershov <codumentary.com@gmail.com>
2014-11-09 00:16:35 +0300

    Add storage.go (renamed store.go)

---
+++ storage.go
@@ -0,0 +1,166 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"fmt"
+	_ "github.com/lib/pq"
+	_ "github.com/mattn/go-sqlite3"
+	"net/http"
+	"regexp"
+	"time"
+)
+
+var POSTGRES_PLACEHOLDERS *regexp.Regexp = regexp.MustCompile("\\$\\d+")
+
+const DELETE_RULE_SQL = `
+DELETE FROM rules
+WHERE site = $1 AND path = $2 AND method = $3
+`
+
+const CREATE_RULE_SQL = `
+INSERT INTO rules
+(host, site, method, headers, delay, response_status, response_body)
+VALUES ($1, $2, $3, $4, $5, $6, $7)
+`
+
+const GET_RULES_SQL = `
+SELECT site, path, method, headers, delay, response_status, response_body
+FROM rules
+WHERE host = $1
+ORDER BY LENGTH(path) DESC, LENGTH(method) DESC
+`
+
+const CREATE_SITE_SQL = `
+INSERT INTO sites
+(site)
+VALUES ($1)
+`
+
+const GET_SITE_SQL = `
+SELECT *
+FROM sites
+WHERE site = $1
+`
+
+type Storage struct {
+	driver     string
+	dataSource string
+	db         *sql.DB
+}
+
+func NewStorage(driver string, dataSource string) (*Storage, error) {
+	db, err := sql.Open(driver, dataSource)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return &Storage{driver: driver, dataSource: dataSource, db: db}, nil
+}
+
+func (storage *Storage) FindRuleMatching(site string, req *http.Request) (rule *Rule, found bool, err error) {
+	rules, err := storage.getSiteRules(site)
+	if err != nil {
+		return nil, false, err
+	}
+	for _, rule := range rules {
+		if rule.Matches(req) {
+			return rule, true, nil
+		}
+	}
+	return nil, false, nil
+}
+
+func (storage *Storage) getSiteRules(site string) ([]*Rule, error) {
+	rules := make([]*Rule, 0)
+	rows, err := storage.db.Query(storage.dialectify(GET_RULES_SQL), site)
+	if err != nil {
+		return rules, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		rule, err := makeRule(rows)
+		if err != nil {
+			return rules, err
+		}
+		rules = append(rules, rule)
+	}
+	return rules, nil
+}
+
+func (storage *Storage) dialectify(sql string) string {
+	if storage.driver == "postgres" {
+		return sql
+	}
+	return POSTGRES_PLACEHOLDERS.ReplaceAllString(sql, "?")
+}
+
+func makeRule(rows *sql.Rows) (*Rule, error) {
+	rule := new(Rule)
+	var headers string
+	var delay int64
+	rows.Scan(&rule.site, &rule.path, &rule.method, &headers, &delay, &rule.responseStatus,
+		&rule.responseBody)
+	rule.delay = time.Duration(delay)
+	var err error
+	rule.headers, err = jsonToMap(headers)
+	return rule, err
+}
+
+func jsonToMap(js string) (map[string]string, error) {
+	object := make(map[string]interface{})
+	err := json.Unmarshal([]byte(js), &object)
+	if err != nil {
+		return nil, err
+	}
+	return makeStringMap(object)
+}
+
+func makeStringMap(object map[string]interface{}) (map[string]string, error) {
+	m := make(map[string]string)
+	for key, value := range object {
+		switch value.(type) {
+		case string:
+			m[key] = value.(string)
+		default:
+			return nil, errors.New(fmt.Sprintf("Expecting string, got %v", value))
+		}
+	}
+	return m, nil
+}
+
+func (storage *Storage) CreateRule(rule *Rule) error {
+  tx, err := storage.db.Begin()
+	_, err = tx.Exec(storage.dialectify(DELETE_RULE_SQL), rule.site, rule.path, rule.method)
+	if err != nil {
+		return err
+	}
+	_, err = tx.Exec(storage.dialectify(CREATE_RULE_SQL), rule.site, rule.path, rule.method,
+		MapToJson(rule.headers), rule.delay, rule.responseStatus, rule.responseBody)
+  err = tx.Commit()
+	return err
+}
+
+func MapToJson(m map[string]string) string {
+	b, _ := json.Marshal(m)
+	return string(b)
+}
+
+func (storage *Storage) CreateSite(domain string) error {
+	_, err := storage.db.Exec(storage.dialectify(CREATE_SITE_SQL), domain)
+	return err
+}
+
+func (storage *Storage) ContainsSite(domain string) (bool, error) {
+	rows, err := storage.db.Query(storage.dialectify(GET_SITE_SQL), domain)
+	if err != nil {
+		return false, err
+	}
+	defer rows.Close()
+	return rows.Next(), nil
+}
