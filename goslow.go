99e70cb75f5fa8e1911325373aeea57206fe8a71
aershov <codumentary.com@gmail.com>
2014-11-30 21:14:03 +0300

    Use half of available CPUs
    Or 1 CPU if there is only one CPU in the system

--- goslow.go
+++ goslow.go
@@ -4,10 +4,14 @@ package main
 
 import (
 	"log"
+	"runtime"
 )
 
 // main starts a slow HTTP server that responds with errors.
 func main() {
+	// GOMAXPROCS call is ignored if NumCPU returns 1 (GOMAXPROCS(0) doesn't change anything)
+	runtime.GOMAXPROCS(runtime.NumCPU() / 2)
+
 	config := NewConfigFromArgs()
 	server := NewServer(config)
 
