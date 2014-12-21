44472fa2f76fa84591ea3a074bcfdf1e84c9365a
aershov <codumentary.com@gmail.com>
2014-12-21 18:03:20 +0300

    Improve formatting of sql statements

--- sql.go
+++ sql.go
@@ -1,10 +1,9 @@
 package main
 
 // To make queries work with both sqlite3 and postgres:
-// string " BYTEA," is replaced with " BLOB," in DDL statements
-// strings "$1", "$2", "$3", ... are replaced with "? in DML statements
+// a) string " BYTEA," is replaced with " BLOB," in DDL statements
+// b) strings "$1", "$2", "$3", ... are replaced with "? in DML statements
 // when using sqlite3 driver.
-
 const (
 	CREATE_SCHEMA_IF_NOT_EXISTS_SQL = `
 CREATE TABLE IF NOT EXISTS sites(
@@ -26,25 +25,28 @@ CREATE TABLE IF NOT EXISTS endpoints (
 
 	DELETE_ENDPOINT_SQL = `
 DELETE FROM endpoints
-WHERE site = $1 AND path = $2 AND method = $3
+WHERE site   = $1
+  AND path   = $2
+	AND method = $3
 `
 
 	INSERT_ENDPOINT_SQL = `
 INSERT INTO endpoints
-(site, path, method, headers, delay, status_code, response)
-VALUES ($1, $2, $3, $4, $5, $6, $7)
+       (site, path, method, headers, delay, status_code, response)
+VALUES ($1,   $2,   $3,     $4,      $5,    $6,          $7)
 `
 
 	GET_SITE_ENDPOINTS_SQL = `
 SELECT site, path, method, headers, delay, status_code, response
 FROM endpoints
 WHERE site = $1
-ORDER BY LENGTH(path) DESC, LENGTH(method) DESC
+ORDER BY LENGTH(path)   DESC,
+         LENGTH(method) DESC
 `
 
 	INSERT_SITE_SQL = `
 INSERT INTO sites
-(site)
+       (site)
 VALUES ($1)
 `
 
